280358fbe5909bcb411186517e11268f1120feab
ushastoe <40743392+krolchonok@users.noreply.github.com>
2023-10-26 10:42:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/krolchonok/OneShot
 cd OneShot
-chmod +x oneshot.py
+chmod +x oneshot.py install.sh launchpixie.sh
 pkg install -y root-repo
 pkg install -y git tsu python wpa-supplicant pixiewps iw openssl
