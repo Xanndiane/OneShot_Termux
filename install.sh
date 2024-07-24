2143a940387c1721673f3c4dda9e8dbef3cf1f0f
Xanndiane <149267560+Xanndiane@users.noreply.github.com>
2024-07-24 21:01:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pkg install -y root-repo
 pkg install -y git tsu python wpa-supplicant pixiewps iw openssl
-git clone https://github.com/krolchonok/OneShot
+git clone https://github.com/Xanndiane/OneShot
 cd OneShot
 chmod +x oneshot.py install.sh launchpixie.sh
 
